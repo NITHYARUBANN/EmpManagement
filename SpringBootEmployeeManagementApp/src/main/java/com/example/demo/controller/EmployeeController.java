def0e819fff30aad16dfa79f82e2d3ebd66ccde2
ruban6 <nithyaruban89341@gmail.com>
2025-01-23 13:02:40 +0530

    Employee App Committed for first time.

--- SpringBootEmployeeManagementApp/src/main/java/com/example/demo/controller/EmployeeController.java
+++ SpringBootEmployeeManagementApp/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
-public class EmployeeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+	@Autowired
+	EmployeeService service;
+	@PostMapping("/save")//http://localhost:8080/employees/save
+	public String saveEmployee(@RequestBody Employee employee) {
+		return service.addEmployee(employee);
+	}
 }
